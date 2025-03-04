5ff5ae66d75a2d3fb6f953ec4ab2ff7055fe78b4
Marcos Willian <markusuuuu@gmail.com>
2025-03-03 22:35:52 -0300

    chore: added required imports

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const path = require("path");
 const { deleteFile, customColorsStyleTag, saveJsonToFile } = require("./helpers");
 const { processTimesheet } = require("../excel-builder/src");
 
