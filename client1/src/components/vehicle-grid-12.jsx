96e0e5f7e42ff37760a1d04ec4bf2c8a5ab59eed
Marcos Willian <markusuuuu@gmail.com>
2025-03-14 22:11:05 -0300

    chore: adjusts layout 1

--- client1/src/components/vehicle-grid-12.jsx
+++ client1/src/components/vehicle-grid-12.jsx
@@ -10,30 +10,30 @@ export function VehicleGrid12() {
           <h2 className="font-medium">Itens Analisados</h2>
         </div>
 
-        <div className="grid grid-cols-3 gap-4 py-6 place-items-center">
+        <div className="grid grid-cols-3 gap-4 py-4 place-items-center">
           <div id="VehicleChunkItem" className="flex flex-col w-full rounded-2xl overflow-hidden bg-[#9D9D9D] shadow-md">
-            <div className="px-4 py-2 text-white">
+            <div className="px-4 py-px text-white">
               <h2 id="vehicleName" className="text-sm font-medium"></h2>
             </div>
 
-            <div id="VehicleGrid12-Image" className="w-full h-[120px] bg-gray-200 bg-cover bg-center" />
+            <div id="VehicleGrid12-Image" className="w-full h-[140px] bg-gray-200 bg-cover bg-center" />
 
-            <div className="px-4 py-2 flex flex-col gap-1 bg-[#9D9D9D]">
+            <div className="px-4 py-px flex flex-col gap-1 bg-[#9D9D9D]">
               <div className="flex items-center gap-2">
                 <div id="VehicleGrid12-SUCCESS" className="bg-[#48BB78] rounded-full p-0.5 hidden">
-                  <Check className="text-white" size={16} />
+                  <Check className="text-white" size={14} />
                 </div>
 
                 <div id="VehicleGrid12-RESTRICTION" className="rounded-full bg-[#F59E0B] p-0.5 hidden">
-                  <IoAlert className="text-white" size={16} />
+                  <IoAlert className="text-white" size={14} />
                 </div>
 
                 <div id="VehicleGrid12-OBSERVATION" className="rounded-full bg-[#3B82F6] p-0.5 hidden">
-                  <AiOutlineInfo className="text-white" size={16} />
+                  <AiOutlineInfo className="text-white" size={14} />
                 </div>
 
                 <div id="VehicleGrid12-FAILED" className="rounded-full bg-[#EF4444] p-0.5 hidden">
-                  <X className="text-white" size={16} />
+                  <X className="text-white" size={14} />
                 </div>
 
                 <span id="vehicleDesc" className="text-sm text-semibold text-white">
